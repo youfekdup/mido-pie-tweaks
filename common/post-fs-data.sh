d2f59f5e302506a1aab8bdbaca3bbc6f2d083dc2
Alex <48605031+youfekdup@users.noreply.github.com>
2019-03-16 20:10:56 +0500

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,4 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
-MODDIR=${0%/*}
-
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+chmod 777 /proc/sys/net/ipv4/ip_no_pmtu_disc
+sysctl -w net.ipv4.ip_no_pmtu_disc=0
+chmod 555 /proc/sys/net/ipv4/ip_no_pmtu_disc
